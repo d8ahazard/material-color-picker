ffb9f59d4cd47836c145f52865bd2115565f46f0
d8ahazard <d8ahazard@gmail.com>
2015-08-13 22:54:27 -0500

    Try to use HSL instead of RGB for sliders.
    Might work better for theme colors.

--- app/src/main/java/com/anjithsasindran/materialcolorpicker/ColorPickerActivity.java
+++ app/src/main/java/com/anjithsasindran/materialcolorpicker/ColorPickerActivity.java
@@ -178,7 +178,7 @@ public class ColorPickerActivity extends Activity implements SeekBar.OnSeekBarCh
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 
             if (display.getRotation() == Surface.ROTATION_0)
-                window.setStatusBarColor(Color.rgb(red, green, blue));
+                window.setStatusBarColor(hue2rgb(red, green, blue));
 
         }
 
@@ -200,6 +200,47 @@ public class ColorPickerActivity extends Activity implements SeekBar.OnSeekBarCh
         Blue = Blue & 0x000000FF; //Mask out anything not blue.
 
         return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
+
+    /**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   Number  h       The hue
+     * @param   Number  s       The saturation
+     * @param   Number  l       The lightness
+     * @return  Array           The RGB representation
+     */
+    public int hslToRgb(int h,int  s,int l){
+
+        int r;
+        int g;
+        int b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+
+
+            int q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            int p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return (Color.rgb((Math.round(r * 255)), (Math.round(g * 255)), (Math.round(b * 255))));
+    }
+
+    public int hue2rgb(int p,int q,int t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return (p + (q - p) * 6 * t);
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
     }
 
     public void colorSelect(View view) {
